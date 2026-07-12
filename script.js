63b415b05433cf7af42f47772e79380a0bfbb481
GlambyBri <simsbriana36@gmail.com>
2026-07-12 09:41:52 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
+const menuToggle = document.getElementById("menu-toggle");
+const navLinks = document.getElementById("nav-links");
 
+menuToggle.addEventListener("click", function () {
+    navLinks.classList.toggle("show");
+});
